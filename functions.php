9a1f18f1f0d614197a981e4a479ac72ffa145b06
itsumoonazicode <dyingkouki@gmail.com>
2021-08-29 22:49:02 +0900

    スタイル読み込み用関数追加

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@ function theme_setup() {
 	add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'theme_setup');
+
+function theme_scripts() {
+	wp_enqueue_style('theme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'theme_scripts');
