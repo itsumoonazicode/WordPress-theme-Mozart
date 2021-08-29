e816f00f7680aed483c733b07076aa033c464db6
itsumoonazicode <dyingkouki@gmail.com>
2021-08-29 17:36:45 +0900

    initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function theme_setup() {
+	add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'theme_setup');
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<body>
+	<?php wp_body_open(); ?>
+	<h1>WordPress Theme "Mozart"</h1>
+	<p>WordPressサイトを作成している最中…。</p>
+</body>
+</html>
