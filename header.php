689705400b5643cb3d85b560962baf7c106f0e60
itsumoonazicode <dyingkouki@gmail.com>
2021-08-29 22:48:41 +0900

    サイトヘッダーを追加

--- header.php
+++ header.php
@@ -5,3 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
+
+<body>
+	<?php wp_body_open(); ?>
+	<header>
+		<h1>Mozart Blog<br><span>雑コード帳</span></h1>
+	</header>
